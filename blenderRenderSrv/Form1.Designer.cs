18350f3fd41eea77806e1f96d5f7e0a28435d339
Jere Tolonen <tolonenjere0@gmail.com>
2026-01-16 19:19:18 +0200

    Add project files.

---
+++ blenderRenderSrv/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace blenderRenderSrv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(489, 293);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(182, 47);
+            this.btnEnable.TabIndex = 0;
+            this.btnEnable.Text = "Start Server";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEnable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnable;
+    }
+}
+
---
+++ blenderRenderSrv/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.Net.Sockets;
+using System.IO;
+
+namespace blenderRenderSrv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+
+            string receiverIp = "192.168.1.50"; 
+            int port = 5000;
+            string filePath = @"C:\path\to\your\file.zip";
+
+            using (TcpClient client = new TcpClient(receiverIp, port))
+            using (NetworkStream nwStream = client.GetStream())
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            {
+                // Stream the file data to the network
+                fs.CopyTo(nwStream);
+                nwStream.Flush();
+                Console.WriteLine("File sent!");
+            }
+        }
+    }
+}
